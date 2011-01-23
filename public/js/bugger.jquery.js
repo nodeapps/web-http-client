c3bc6d16a7f6e610b42790d02ccae40800642e28
Marak Squires <marak.squires@gmail.com>
2011-01-23 13:23:48 -0500

    [ui] Added several new features to UI.

--- public/js/bugger.jquery.js
+++ public/js/bugger.jquery.js
@@ -1,3 +1,46 @@
 // this is the bugger widget
 
 var bugger = {};
+bugger.request = function(options, callback){
+  
+  // perform jQuery ajax request
+  $.ajax({
+     url:   '/request',
+     type:  'POST',
+     data:   options,
+
+     // after the XHR has successfully completed, process it
+     success: function(data, textStatus, XMLHttpRequest){
+       console.log('this is success');
+       callback( null, data );
+     },
+     error: function(XMLHttpRequest, textStatus, errorThrown){
+       console.log('this is an error');
+       console.log(errorThrown);
+       callback('this is an error');
+     }
+     
+  });
+
+};
+
+bugger.headers = [
+ "Accept",
+ "Accept-Language",
+ "Accept-Encoding",
+ "Cache-Control",
+ "Cookie",
+ "Connection",
+ "Content-Length",
+ "Content-Type",
+ "From",
+ "Host",
+ "If-Match",
+ "If-Modified-Since",
+ "Host",
+ "If-None-Match",
+ "If-Range",
+ "If-Unmodified-Since",
+ "Pragma",
+ "User-Agent"
+];
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var ws = require('webservice'),
     http = require('http'),
     bugger = require('./lib/bugger');
 
-    
 http.createServer(require('stack')(
+  require('./lib/stack/request.stack')(),
   require('./lib/stack/webservice.stack')(),
   require('./lib/stack/static.stack')()
 )).listen(8080);
