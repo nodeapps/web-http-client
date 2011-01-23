07ec5a72c7d52ae8276e198117b89554e1958e0c
Marak Squires <marak.squires@gmail.com>
2011-01-23 16:49:09 -0500

    Updating for deploy

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('hello, i know nodejitsu.')
-  res.end();
-}).listen(80);
-
-/*
 http.createServer(require('stack')(
   require('./lib/stack/request.stack')(),
   require('./lib/stack/webservice.stack')(),
   require('./lib/stack/static.stack')()
 )).listen(8080);
-*/
 
 console.log('> bugger started on port 8080');
 
